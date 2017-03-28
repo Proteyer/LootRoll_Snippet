4c0c34245faf206eaf461a5e27c8fcc8848c7953
Proteyer <proteyer@hotmail.de>
2017-03-28 09:47:34 +0200

    Made rolledStuff table working as expected

--- depot.lua
+++ depot.lua
@@ -1,29 +1,25 @@
 local instanceID = 555666
+local itemID = 999999
 local countedLoot = nil
 
-local rolledStuff = {
-	itemID, allocation = {itemLink, dispatcher, winner}, rolls = {name, rolled, maxroll}
-}
+local rolledStuff = {} --	itemID, allocation = {lootMaster, dispatcher, itemLink, winner}, rolls = {name, rolled, maxroll}
 
-rolledStuff[instanceID] = {
-	{
-		999999,
-		{
-			{ "|cXXXXXXXX|", "Kerbaal-DieAldor", "Aiska-DieAldor" },
-			{ "|cXXXXXXXX|", "Tyurru-DieAldor" }
-		},
-		{
-			{ "Squip-DieAldor", 35, 50},
-			{ "Aiska-DieAldor", 15, 100}
-		}
-	}
-}
+rolledStuff[555666] = {}
+rolledStuff[555666][999999] = {}
+rolledStuff[555666][999999]["allocation"] = {}
+rolledStuff[555666][999999]["allocation"][1] = { "Nagisa-DieAldor", "Kerbaal-DieAldor", "|cXXXXXXXX|", "Aiska-DieAldor" }
+rolledStuff[555666][999999]["allocation"][2] = { "Nagisa-DieAldor", "Tyurru-DieAldor", "|cXXXXXXXX|", }
+rolledStuff[555666][999999]["rolls"] = {}
+rolledStuff[555666][999999]["rolls"][1] = { "Aiska-DieAldor", 78, 100 }
+rolledStuff[555666][999999]["rolls"][2] = { "Squip-DieAldor", 25, 50 }
 
 function printDispatcher(ID)
 	local i = 1
-	while rolledStuff[ID][1][2][i] do
-		print(rolledStuff[ID][1][2][i][2])
+	while i <= #ID do
+		print(ID[i][2])
 		i = i + 1
 	end
 end
-printDispatcher(instanceID)
+
+--print(rolledStuff[instanceID][itemID]["allocation"][1][2])
+printDispatcher(rolledStuff[instanceID][itemID]["allocation"])
